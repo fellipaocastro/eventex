d8fe5b01b6f1c33cf82de6f92b618f6c7471cf77
Fellipe Castro <contact@fellipecastro.com>
2014-03-05 22:05:04 -0300

    Atualiza repositório

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'eventex.core.views.home', name='home'),
+    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
