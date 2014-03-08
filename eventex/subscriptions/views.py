b3f1c37eefd947cc319e8bb748ec9fc0be149473
Fellipe Castro <contact@fellipecastro.com>
2014-03-08 12:43:32 -0300

    Subscription

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 from django.http import HttpResponseRedirect
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -27,4 +28,6 @@ def create(request):
 
 
 def detail(request, pk):
-    return HttpResponse()
+    subscription = get_object_or_404(Subscription, pk=pk)
+    return render(request, 'subscriptions/subscription_detail.html',
+                    {'subscription': subscription})
--- eventex/urls.py
+++ eventex/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'eventex.core.views.home', name='home'),
     url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
-    url(r'ˆinscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
+    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
