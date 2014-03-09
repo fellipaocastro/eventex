c67a463d6b882db22487d51274173cfb563b894f
Fellipe Castro <contact@fellipecastro.com>
2014-03-09 19:27:27 -0300

    Forms features

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -25,12 +25,23 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(email='')
         self.assertFalse(form.errors)
 
+    def test_must_inform_email_or_phone(self):
+        'Email and Phone are optional, but one must be informed.'
+        form = self.make_validated_form(email='', phone_0='', phone_1='')
+        self.assertItemsEqual(['__all__'], form.errors)
+
+    def test_name_must_be_capitalized(self):
+        'Name must be capitalized.'
+        form = self.make_validated_form(name='FELLIPE castro')
+        self.assertEqual('Fellipe Castro', form.cleaned_data['name'])
+
     def make_validated_form(self, **kwargs):
         data = dict(
             name='Fellipe Castro',
             email='contact@fellipecastro.com',
             cpf='12345678901',
-            phone='21-996186180'
+            phone_0='21',
+            phone_1='996186180'
         )
         data.update(kwargs)
         form = SubscriptionForm(data)
