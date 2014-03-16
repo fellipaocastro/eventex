f9054d31dc5eb2d771cbe806fbeb0cfc20497d36
Fellipe Castro <contact@fellipecastro.com>
2014-03-16 09:43:14 -0300

    Speaker e Contact

--- eventex/core/tests/test_models.py
+++ eventex/core/tests/test_models.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
 from eventex.core.models import Speaker
+from eventex.core.models import Contact
 
 
 class SpeakerModelTest(TestCase):
@@ -20,3 +22,46 @@ class SpeakerModelTest(TestCase):
     def test_unicode(self):
         'Speaker string representation should be the name.'
         self.assertEqual(u'Fellipe Castro', unicode(self.speaker))
+
+
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            name='Fellipe Castro',
+            slug='fellipe-castro',
+            url='http://fellipecastro.com',
+            description='Passionate software developer!')
+
+    def test_email(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind='E',
+            value='contact@fellipecastro.com')
+        self.assertEqual(1, contact.pk)
+
+    def test_phone(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind='P',
+            value='21-996186180')
+        self.assertEqual(1, contact.pk)
+
+    def test_fax(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind='F',
+            value='21-12345678')
+        self.assertEqual(1, contact.pk)
+
+    def test_kind(self):
+        'Contact kind should be limited to E, P or F.'
+        contact = Contact(speaker=self.speaker, kind='A', value='B')
+        self.assertRaises(ValidationError, contact.full_clean)
+
+    def test_unicode(self):
+        'Contact string representation should be value'
+        contact = Contact(
+            speaker=self.speaker,
+            kind='E',
+            value='contact@fellipecastro.com')
+        self.assertEqual(u'contact@fellipecastro.com', unicode(contact))
